5f8c433dfd9bfdb1395a3cfe89e5cac82665dc36
Володьков Иван Андреевич <IAVolodkov@sberbank.ru>
2020-03-17 14:24:23 +0300

    initial commit

---
+++ app/src/main/java/com/example/currencyconverter/ui/main/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.example.currencyconverter.ui.main
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import com.example.currencyconverter.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+
+}
