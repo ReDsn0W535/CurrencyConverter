dc15126ba251eb9fb5b54d435e4c8f837a128338
Володьков Иван Андреевич <IAVolodkov@sberbank.ru>
2020-03-20 01:51:39 +0300

    added currency recyclerView adapter + ViewHolder

---
+++ app/src/main/java/com/example/currencyconverter/ui/recycler/CurrencyRecyclerAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.currencyconverter.ui.recycler
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.currencyconverter.R
+
+class CurrencyRecyclerAdapter(private val currencies : List<String>) : RecyclerView.Adapter<CurrencyRecyclerAdapter.CurrencyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrencyViewHolder {
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.currency, parent, false)
+        return CurrencyViewHolder(view)
+    }
+
+    override fun getItemCount() = currencies.size
+
+    override fun onBindViewHolder(holder: CurrencyViewHolder, position: Int) {
+        val currency = currencies[position]
+        holder.bind(currency)
+
+    }
+
+
+    inner class CurrencyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+        fun bind(currency: String){
+            itemView.findViewById<TextView>(R.id.currency_name).text = currency
+        }
+    }
+}
